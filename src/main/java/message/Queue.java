32584c3b606403ea964c9037c212ce9565dc8481
Clay Press <press0@gmail.com>
2014-01-07 11:41:31 -0600

    terse form

--- src/main/java/message/Queue.java
+++ src/main/java/message/Queue.java
@@ -10,16 +10,13 @@ import java.util.concurrent.BlockingQueue;
 public class Queue {
 
     public static void main(String[] args) {
-        final int COUNT = 50;
+        final int COUNT = 5;
         final int SECONDS_TIMEOUT = 3;
 
         BlockingQueue<Message> queue = new ArrayBlockingQueue<>(COUNT * 2);
-        Generator generator = new Generator(queue, COUNT);
-        Reader consumer = new Reader(queue, SECONDS_TIMEOUT);
-        new Thread(generator).start();
-        new Thread(consumer).start();
-        generator = new Generator(queue, COUNT);
-        new Thread(generator).start();
+        new Thread(new Generator(queue, COUNT)).start();
+        new Thread(new Reader(queue, SECONDS_TIMEOUT)).start();
+        new Thread(new Generator(queue, COUNT)).start();
     }
 
 }
